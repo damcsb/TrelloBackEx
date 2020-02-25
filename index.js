8b8b4fd0147cc672ac4f9164d1664503fd78c7e5
damcsb <48583917+damcsb@users.noreply.github.com>
2020-02-25 13:22:37 +0100

    mongoose/mongo connection

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ var Server = /** @class */ (function () {
         this.config();
     }
     Server.prototype.config = function () {
+        //
+        ////Mongo Connection
+        var MONGO_URI = 'mongodb+srv://root:root@trelloex-4nzrj.mongodb.net/test?retryWrites=true&w=majority';
+        var MongoClient = require('mongodb').MongoClient;
+        var client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(function () {
+            console.log("CONNECTED");
+        });
+        //
+        ////Set App-Port
         this.app.set('port', process.env.PORT || 3000);
     };
     Server.prototype.start = function () {
+        //
+        ////Server start method
         this.app.listen(this.app.get('port'), function () {
             console.log('Server starting');
         });
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express = require('express');
-
 import https = require('https');
+import mongoose = require('mongoose');
+
 
 
 class Server {
 
     public app: express.Express = express();
+
+
 
     constructor() {
         this.app = express();
@@ -13,15 +16,28 @@ class Server {
     }
 
     config() {
+        //
+        ////Mongo Connection
+        const MONGO_URI = 'mongodb+srv://root:root@trelloex-4nzrj.mongodb.net/test?retryWrites=true&w=majority';
+        const MongoClient = require('mongodb').MongoClient;
+        const client = new MongoClient(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(()=>{
+            console.log("CONNECTED")
+        });
+        
+
+        //
+        ////Set App-Port
         this.app.set('port', process.env.PORT || 3000);
     }
 
     start() {
+        //
+        ////Server start method
         this.app.listen(this.app.get('port'), () => {
             console.log('Server starting');
         })
     }
-
 }
 const server = new Server();
 server.start();
